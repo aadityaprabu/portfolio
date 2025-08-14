4c672738b9fd3a2a7710da89d89356ab2dceb926
aaditya prabu <aadityaprabu@gmail.com>
2025-08-14 23:52:36 +0530

    Add backend app

---
+++ app/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Simple route
+app.get("/", (req, res) => {
+  res.send("✅ Express server is running!");
+});
+
+// Test API endpoint
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Express!", time: new Date().toISOString() });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
